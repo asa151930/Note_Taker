fcdfb7eb786f0991291b7982b59dfa9d8d381691
Ali Arab <aliarab812@yahoo.com>
2021-04-20 23:49:09 -0500

    server is up and running

--- server.js
+++ server.js
@@ -2,20 +2,18 @@
 const http = require('http');
 
 // Define a port to listen for incoming requests
-const PORT = 8080;
+const PORT = 5000;
 
-// Create a generic function to handle requests and responses
-const handleRequest = (request, response) => { // when its called by http server it looks for request and response object. 
-  // Send the below string to the client when the user visits the PORT URL
-  response.end(`It Works!! Path Hit: ${request.url}`);
+const handleRequest = (request, response) => {  
+  
+  response.end(`The path is LIVE!! ${request.url}`);
 };
 
-// Use the Node HTTP package to create our server.
-// Pass the handleRequest function to empower it with functionality.
+
 const server = http.createServer(handleRequest);
 
-// Start our server so that it can begin listening to client requests.
-server.listen(PORT, () => { // listen function needs a port number created above. 
-  // Log (server-side) when our server has started
-  console.log(`Server listening on: http://localhost:${PORT}`); // means we're up and running!!!
+
+server.listen(PORT, () => {  
+  
+  console.log(`Server listening on: http://localhost:${PORT}`); 
 });
