85d814b45bf5542ea81bf3407bec84ac6252a8fc
Ali Arab <aliarab812@yahoo.com>
2021-04-21 00:15:37 -0500

    notes

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(__dirname));
 
+// Site is live..
 app.listen(PORT, function () {
     console.log("App listening on PORT: " + PORT);
 });
