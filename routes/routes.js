394c204c05422a4cd64a2c1a5da15a7ae7d85567
Ali Arab <aliarab812@yahoo.com>
2021-04-21 00:51:36 -0500

    working

--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,25 @@
 const fs = require('fs');
-const path = require('path');  
+const path = require('path'); 
 
+module.exports = app => {
 
+fs.readFile(`${__dirname}/db/db.json`, (err, data) => {
+    if (err) throw err;
+    
+});
 // Set up api routes 
+app.get('/api/notes', function (req, res){
+    res.json(notes);
+});
+
+// Create new notes - takes in JSON input
+app.post ('/api/notes', function (req, res){
+    const newNote = req.body;
+    notes.push(newNote);
+    res.json(newNote);
+    console.log('Added a new note: ' + newNote);
+});
 
 
 
-
+}
